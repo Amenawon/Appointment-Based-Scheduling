c3ae7d86b9827b8abff6c70406e5513a00b05efd
ValeriyaMix <lerka-mishka@yandex.ru>
2024-12-29 20:33:08 +0000

    Create an Appointment model.

---
+++ AppointmentBasedScheduling/WebApi/Models/AppointmentModel.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApi.Models
+{
+    public class AppointmentModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan Duration { get; set; }
+        public string Location { get; set; }
+        public Status Status { get; set; }
+        public List<AttendeeModel> Attendees { get; set; }
+
+        public UserModel CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
