a956d056cff34f8eebc42a2ebecc34134ef7197a
ValeriyaMix <lerka-mishka@yandex.ru>
2024-12-29 20:32:28 +0000

    Create a controller for the appointment entity.

---
+++ AppointmentBasedScheduling/WebApi/Controllers/AppointmentController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentController : ControllerBase
+    {
+        private readonly IAppointmentData _appontmentData;
+
+        public AppointmentController(IAppointmentData appontmentData)
+        {
+            _appontmentData = appontmentData;
+        }
+
+        [HttpGet]
+        public List<AppointmentModel> Get()
+        {
+            return _appontmentData.GetAppointment();
+        }
+
+        [HttpPost]
+        public void Post(AppointmentModel appointment)
+        {
+            _appontmentData.SaveInventoryRecord(appointment);
+        }
+    }
+}
