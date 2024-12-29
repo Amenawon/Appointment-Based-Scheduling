e253f320eb782faaddd0251b3cc50566b1d29b45
ValeriyaMix <lerka-mishka@yandex.ru>
2024-12-29 20:47:56 +0000

    Add a model for an Attendee.

---
+++ AppointmentBasedScheduling/WebApi/Models/AttendeeModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi.Models
+{
+    public class AttendeeModel
+    {
+        public string? UserId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool IsRegistered { get; set; }
+    }
+}
