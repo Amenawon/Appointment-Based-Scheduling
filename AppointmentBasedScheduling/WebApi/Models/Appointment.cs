40855048d4e1ea267c718742b8f54aa9d91679d0
ValeriyaMix <lerka-mishka@yandex.ru>
2025-01-15 22:38:13 +0000

    Remove a connection between the Appointment table and the Attendees table for now. Create a migration file and run it.

--- AppointmentBasedScheduling/WebApi/Models/Appointment.cs
+++ AppointmentBasedScheduling/WebApi/Models/Appointment.cs
@@ -8,10 +8,10 @@ namespace WebApi.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime Date { get; set; }
-        public TimeSpan Duration { get; set; }
+        public int Duration { get; set; }
         public string Location { get; set; }
         public Status Status { get; set; }
-        public List<Attendee> Attendees { get; set; }
+        public int Attendees { get; set; }
 
         public string OrganiserId { get; set; }
         public DateTime CreateDate { get; set; }
