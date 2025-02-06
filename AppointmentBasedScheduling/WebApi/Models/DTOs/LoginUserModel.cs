914119c6047e80a1522de2dddac4de23e0d43c94
ValeriyaMix <lerka-mishka@yandex.ru>
2025-02-06 21:48:27 +0000

    Add LoginUserModel DTO for the Login method in the user controller.

---
+++ AppointmentBasedScheduling/WebApi/Models/DTOs/LoginUserModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models.DTOs
+{
+    public class LoginUserModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
