a61866963b27dcb3ac689a4cf8f94fcc83ee4ce7
ValeriyaMix <lerka-mishka@yandex.ru>
2025-02-06 21:16:49 +0000

    Add User DTOs properties and add the register method for the user controller.

---
+++ AppointmentBasedScheduling/WebApi/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+using WebApi.Models.DTOs;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+
+        public async Task<IActionResult> RegisterNewUserAccount([FromBody] UserRegisterModel userRegisterModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = new User
+            {
+                UserName = userRegisterModel.Email,
+                Email = userRegisterModel.Email,
+                FirstName = userRegisterModel.FirstName,
+                LastName = userRegisterModel.LastName,
+                Organisation = userRegisterModel.Organisation
+            };
+            var result = await _userManager.CreateAsync(user);
+
+            if (result.Succeeded)
+            {
+                return Ok("New user registered successfully!");
+            }
+
+            return BadRequest(result.Errors);
+        }
+    }
+}
--- AppointmentBasedScheduling/WebApi/Models/DTOs/UserRegisterModel.cs
+++ AppointmentBasedScheduling/WebApi/Models/DTOs/UserRegisterModel.cs
@@ -1,7 +1,22 @@
-﻿namespace WebApi.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models.DTOs
 {
     public class UserRegisterModel
     {
+        [Required]
+        public string FirstName { get; set; }
 
+        [Required]
+        public string LastName { get; set; }
+
+        public string? Organisation { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
     }
 }
