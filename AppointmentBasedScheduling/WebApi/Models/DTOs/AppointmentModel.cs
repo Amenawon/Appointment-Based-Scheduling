645a736d51aeb669780f3a364e30d17e5eb833fa
ValeriyaMix <lerka-mishka@yandex.ru>
2025-02-25 21:39:05 +0000

    Create an Appointment DTO.

---
+++ AppointmentBasedScheduling/WebApi/Models/DTOs/AppointmentModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApi.Models.DTOs
+{
+    public class AppointmentModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public int Duration { get; set; }
+        public string Location { get; set; }
+        public string Status { get; set; }
+        public string OrganiserEmail { get; set; }
+        public List<string> ListAttendeesEmails { get; set; }
+    }
+}
